227f055bc9b14dad9e486ddd5fc33f14f6dddd62
brendeny <46551861+brendeny@users.noreply.github.com>
2019-01-11 17:52:00 -0800

    Game1 Header

---
+++ game1_h.h
@@ -0,0 +1,18 @@
+#pragma once
+
+//game1 header
+
+//Game1 is the start of game
+void game1();
+
+//Gameif is for the ifelse statement used
+void game1if();
+
+//Game1_1 is part 1 of Game1
+void game1_1();
+
+//Game1_2 is part 2 of Game1
+void game1_2();
+
+//Game 1_3 is part 3 of Game1
+void game1_3();
